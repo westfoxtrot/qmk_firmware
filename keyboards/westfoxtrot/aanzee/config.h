cd3732f46a6ee1b6d9d93d23742909a79fcbc4df
westfoxtrot <max@cablecardesigns. co>
2019-01-27 14:49:17 +0100

    finished cleaning up rgbw support

--- keyboards/westfoxtrot/aanzee/config.h
+++ keyboards/westfoxtrot/aanzee/config.h
@@ -47,15 +47,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN E6
 #ifdef RGB_DI_PIN
    #define RGBLED_NUM 9
-   #define RGBLIGHT_HUE_STEP 8
-   #define RGBLIGHT_SAT_STEP 8
-   #define RGBLIGHT_VAL_STEP 8
+   #define RGBLIGHT_HUE_STEP 12
+   #define RGBLIGHT_SAT_STEP 25
+   #define RGBLIGHT_VAL_STEP 12
    #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
    #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
- /*== all animations enable ==*/
+/*== all animations enable ==*/
    #define RGBLIGHT_ANIMATIONS
- /*== or choose animations ==*/
-//   #define RGBLIGHT_EFFECT_BREATHING
+/*== or choose animations ==*/
+//  #define RGBLIGHT_EFFECT_BREATHING
 //   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 //   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //   #define RGBLIGHT_EFFECT_SNAKE
@@ -65,6 +65,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //   #define RGBLIGHT_EFFECT_RGB_TEST
 //   #define RGBLIGHT_EFFECT_ALTERNATING
 #endif
+#define RGBW
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCING_DELAY 5
