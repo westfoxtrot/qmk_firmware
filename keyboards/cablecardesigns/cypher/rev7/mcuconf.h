e43450d1d4019e268781c8016493245ac85ed74f
westfoxtrot <17062872+westfoxtrot@users.noreply.github.com>
2025-03-28 11:47:26 +0100

    update for latest qmk rev

--- keyboards/cablecardesigns/cypher/rev7/mcuconf.h
+++ keyboards/cablecardesigns/cypher/rev7/mcuconf.h
@@ -4,9 +4,11 @@
 
 #include_next <mcuconf.h>
 
-#undef STM32_PWM_USE_TIM3
-#define STM32_PWM_USE_TIM3 TRUE
 #undef STM32_PWM_USE_TIM1
 #define STM32_PWM_USE_TIM1 TRUE
+
+#undef STM32_PWM_USE_TIM3
+#define STM32_PWM_USE_TIM3 TRUE
+
 #undef STM32_PWM_USE_ADVANCED
 #define STM32_PWM_USE_ADVANCED TRUE
